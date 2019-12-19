01ed3122e224b61fe65178238473f33da45ff51a
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-19 10:45:30 -0500

    filling out unreal file more

--- portfolio/src/components/home/UnrealEngine.js
+++ portfolio/src/components/home/UnrealEngine.js
@@ -8,10 +8,12 @@ export default function Artwork() {
         <div className='projectcontainer'>
             <div className='projectUnreal'>
                 <h2 className='textcolor'>Side Scroller</h2>
-                    <p className='textcolor'>Project description
+                    <p className='textcolor'>Factory
                         <br/>
                         <br/>
-                        <li>bullet point</li>
+                        <li>Factory animation</li>
+                        <li>3D Studio Max</li>
+                        <li>Photshop</li>
                     </p>
                     <div>
                         <button class="btnU">Github code</button>
@@ -21,9 +23,32 @@ export default function Artwork() {
                 frameborder="0" allow="accelerometer; autoplay; 
                 encrypted-media; gyroscope; picture-in-picture" 
                 allowfullscreen></iframe>
-
             </div>
+
+            <div className='projectUnreal'>
+                <h2 className='textcolor'>Ship Side Scroller</h2>
+                    <p className='textcolor'>You are a modern day ship, that is fighting
+                    enemy submarines. You can shoot missles at air, and drop depth charges
+                    against submarines. 
+                        <br/>
+                        <br/>
+                        <li>Upgrade ships health/appearance</li>
+                        <li>weapons</li>
+                        <li>enemies get harder with each level</li>
+                        <li>Enemy Bosses</li>
+                    </p>
+                    <div>
+                        <button class="btnU">Github code</button>
+                    </div>
+                <iframe width="560" height="315" 
+                src="https://www.youtube.com/embed/-Tm8T6OvcI0" 
+                frameborder="0" allow="accelerometer; autoplay; 
+                encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen></iframe>
+            </div>
+
         </div> 
+  
     </div>  
     )
 }
